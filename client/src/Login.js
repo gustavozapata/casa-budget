1819bf6be048fd74debba8f931bd23b77354ec67
Gustavo Zapata <tavordie@hotmail.com>
2022-01-30 01:19:26 +0000

    add protected router on login and store from path visited

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,13 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { login } from "./store/appSlice";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const isLogged = useSelector((state) => state.app.isLogged);
   const serverError = useSelector((state) => state.app.serverError);
   const dispatch = useDispatch();
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  let from = location.state?.from?.pathname || "/";
+
+  useEffect(() => {
+    if (isLogged) {
+      navigate(from, { replace: true });
+    }
+  }, [isLogged, navigate, from]);
 
   return (
     <div className="Login">
@@ -30,9 +42,7 @@ const Login = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
       </div>
-      <Link to="/">
-        <button onClick={() => dispatch(login(email, password))}>Login</button>
-      </Link>
+      <button onClick={() => dispatch(login(email, password))}>Login</button>
       <p className="error-msg">{serverError}</p>
     </div>
   );
--- client/src/layout/PrivateLayout.js
+++ client/src/layout/PrivateLayout.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Navigate } from "react-router";
 
 const PrivateRoute = () => {
   const isLogged = useSelector((state) => state.app.isLogged);
+  let location = useLocation();
 
   return isLogged ? (
     <div className="PrivateLayout">
       <Outlet />
     </div>
   ) : (
-    <Navigate to="/login" />
+    <Navigate to="/login" state={{ from: location }} />
   );
 };
 
