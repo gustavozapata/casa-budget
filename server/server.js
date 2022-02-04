0dbd29bd62b95fa15326566d991145e5a354d55e
Ubuntu <ubuntu@ubuntu.broadband>
2022-02-04 12:38:15 +0000

    switch to mongodb local and enable ssl on ubuntu server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const DB = process.env.DB_URI.replace("<PASSWORD>", process.env.DB_PASSWORD);
+// const DB = process.env.DB_URI.replace("<PASSWORD>", process.env.DB_PASSWORD);
+const DB = process.env.DB_URI_LOCAL
 
 mongoose
   .connect(DB, {
